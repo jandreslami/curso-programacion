d4c7c3dbf03013c83fe13c6f2f5ad7469e81bc17
jandreslami <115507664+jandreslami@users.noreply.github.com>
2022-11-09 11:41:46 -0300

    Agrega tarea 3 clase 5 y modifica .gitignore
    Se ocultan los archivos que son ***-mio.***
    
    La tarea 3 clase 5 está incompleta

---
+++ tareas/clase-5/tarea-3-clase-5.js
@@ -0,0 +1,104 @@
+
+//TAREA: En otro archivo distinto,
+// Crear una lista de <ol> y <li> que contengan sólo números.
+// Convertir esos números a un array y:
+// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
+// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
+// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
+// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+
+const $textoPromedio = document.querySelector("#promedio")
+const $textoNumeroMasPequenio = document.querySelector("#pequenio")
+const $textoNumeroMasGrande = document.querySelector("#grande")
+const $textoNumeroMasFrecuente = document.querySelector("#frecuente")
+
+let lista = document.querySelectorAll("li");
+
+let arrayNumeros = [];
+
+// Pasa la OL a un Array
+for ( i=0 ; i < lista.length ; i++ ) {
+
+    let numero = lista[i].innerHTML; 
+    arrayNumeros[i] = Number(numero);
+}
+
+
+function calcularPromedio (array){
+    
+    let acumulador = 0
+
+    for (i=0 ; i< array.length ; i++){
+        acumulador = acumulador + array[i]
+    }
+    
+    return acumulador / array.length
+}
+
+
+function calcularMenorNumero (array) {
+    
+    let acumulador = array[0];
+
+    for  (i=0; i<array.length ; i++) {
+
+        if (array[i] < acumulador) {
+            acumulador = array[i]
+        }
+    }
+
+    return acumulador
+}
+
+function calcularMayorNumero (array){
+
+    let acumulador = array[0];
+
+    for  (i=0; i<array.length ; i++) {
+
+        if (array[i] > acumulador) {
+            acumulador = array[i]
+        }
+    }
+
+    return acumulador
+
+}
+
+function calcularModa (array) {
+
+    let contadorMax = 0
+
+    let contadorMin = 0 
+
+    for  (i=1; i<array.length ; i++) {
+
+        let comparador = array [i]
+
+        for (i=array.length ; i>=0;  i--) {
+            
+           if(comparador === array[i]) {
+            contadorMax++
+           }
+            
+        }
+        
+        }
+    }
+
+    for (i=array.length ; i>=0;  i--) {
+        
+        //console.log (arrayNumeros[i])
+    }
+
+
+
+}
+
+$textoPromedio.innerHTML = calcularPromedio(arrayNumeros);
+$textoNumeroMasPequenio.innerHTML = calcularMenorNumero(arrayNumeros);
+$textoNumeroMasGrande.innerHTML = calcularMayorNumero(arrayNumeros);
+
+calcularModa (arrayNumeros)
+
--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -64,10 +64,3 @@ Ejemplo form:
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
 
-//TAREA: En otro archivo distinto,
-// Crear una lista de <ol> y <li> que contengan sólo números.
-// Convertir esos números a un array y:
-// 1. calcular el promedio y mostrarlo en un <em> pre-creado con el texto "El promedio es..."
-// 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
-// 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
-// 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
